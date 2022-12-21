2a12b6740348430f02e663d220701af95de72d5f
Raquel Murcia <68744281+ramurjua@users.noreply.github.com>
2022-12-21 14:40:54 +0100

    problemas linealizacion

--- Modelo H8/1 GDL/linealizacion_hemsH8.m
+++ Modelo H8/1 GDL/linealizacion_hemsH8.m
@@ -1,5 +1,18 @@
-x=[0.0225, 0];
-argout=linmod('H8_1GDL_currentinput',x,0.019);
+%% Con LUT
+
+x=[0.019, 0]; % estados
+argout=linmod('H8_1GDL_currentinput',x,0); % entrada
+A=argout.a;
+B=argout.b;
+C=argout.c;
+D=argout.d;
+sys=ss(A,B,C,D);
+sysr=minreal(sys)
+
+%% Con ecuaciones
+
+x=[0.019, 0]; % estados
+argout=linmod('H8_1GDL_currentinput_ecuaciones',x,0); % entrada
 A=argout.a;
 B=argout.b;
 C=argout.c;
