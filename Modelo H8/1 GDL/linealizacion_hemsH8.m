34d2ce325ff6dee9b7a5d3f43047ff8f1e6d7037
Raquel Murcia <68744281+ramurjua@users.noreply.github.com>
2022-12-22 15:49:24 +0100

    actualizada distancia de levitacion a 23

--- Modelo H8/1 GDL/linealizacion_hemsH8.m
+++ Modelo H8/1 GDL/linealizacion_hemsH8.m
@@ -1,6 +1,6 @@
 %% Con LUT
 
-x=[0.019, 0]; % estados
+x=[0.023, 0]; % estados
 argout=linmod('H8_1GDL_currentinput',x,0); % entrada
 A=argout.a;
 B=argout.b;
@@ -11,11 +11,11 @@ sysr=minreal(sys)
 
 %% Con ecuaciones
 
-x=[0.019, 0]; % estados
+x=[0.023, 0]; % estados
 argout=linmod('H8_1GDL_currentinput_ecuaciones',x,0); % entrada
-A=argout.a;
-B=argout.b;
-C=argout.c;
-D=argout.d;
-sys=ss(A,B,C,D);
-sysr=minreal(sys)
+Ap=argout.a;
+Bp=argout.b;
+Cp=argout.c;
+Dp=argout.d;
+sysp=ss(Ap,Bp,Cp,Dp);
+sysrp=minreal(sysp)
