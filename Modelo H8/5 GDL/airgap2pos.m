f72adb0ddaa4dc30e4e90de53d7ce5d3be1b01af
pech422 <alvarosanjo04@gmail.com>
2022-12-26 12:01:01 +0100

    Update airgap2pos.m

--- Modelo H8/5 GDL/airgap2pos.m
+++ Modelo H8/5 GDL/airgap2pos.m
@@ -8,11 +8,11 @@ y_LIM = %falta
 z_LIM = %falta
 
 % ROTACIONES
-rotx_aux(1) = asin((airgaps(1) - airgaps(2))/(2 * y_LEV));
-rotx_aux(2) = asin((airgaps(3) - airgaps(4))/(2 * y_LEV));
+rotx_aux(1) = asin((airgaps(1) - airgaps(2))/(y_LEV(1) + y_LEV(2)));
+rotx_aux(2) = asin((airgaps(3) - airgaps(4))/(y_LEV(3) + y_LEV(4)));
 
-roty_aux(1) = asin((airgaps(3) - airgaps(1))/(2 * x_LEV));
-roty_aux(2) = asin((airgaps(4) - airgaps(2))/(2 * x_LEV));
+roty_aux(1) = asin((airgaps(3) - airgaps(1))/(x_LEV(3) + x_LEV(1)));
+roty_aux(2) = asin((airgaps(4) - airgaps(2))/(x_LEV(4) + x_LEV(2)));
 
 rotz_aux(1) = asin((airgaps(5) - airgaps(7))/(2 * x_LIM));
 rotz_aux(2) = asin((airgaps(6) - airgaps(8))/(2 * x_LIM));
@@ -28,10 +28,10 @@ y_aux(2) = - airgaps(6) + y_LIM * cos(rotz) - x_LIM * sin(rotz);
 y_aux(3) = + airgaps(7) - y_LIM * cos(rotz) + x_LIM * sin(rotz);
 y_aux(4) = - airgaps(8) + y_LIM * cos(rotz) + x_LIM * sin(rotz);
 
-z_aux(1) = airgaps(1)  + z_LEV * cos(roty) + x_LEV * sin(roty) - y_LEV * sin(rotx);
-z_aux(2) = airgaps(2)  + z_LEV * cos(roty) + x_LEV * sin(roty) + y_LEV * sin(rotx);
-z_aux(3) = airgaps(3)  + z_LEV * cos(roty) - x_LEV * sin(roty) - y_LEV * sin(rotx);
-z_aux(4) = airgaps(4)  + z_LEV * cos(roty) - x_LEV * sin(roty) + y_LEV * sin(rotx);
+z_aux(1) = airgaps(1)  + z_LEV(1) * cos(roty) + x_LEV(1) * sin(roty) - y_LEV(1) * sin(rotx);
+z_aux(2) = airgaps(2)  + z_LEV(2) * cos(roty) + x_LEV(2) * sin(roty) + y_LEV(2) * sin(rotx);
+z_aux(3) = airgaps(3)  + z_LEV(3) * cos(roty) - x_LEV(3) * sin(roty) - y_LEV(3) * sin(rotx);
+z_aux(4) = airgaps(4)  + z_LEV(4) * cos(roty) - x_LEV(4) * sin(roty) + y_LEV(4) * sin(rotx);
 
 y = mean(y_aux);
 z = mean(z_aux);
