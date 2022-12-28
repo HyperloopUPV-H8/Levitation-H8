fa1b04d2d32cdcc284c87fca5da2cc0ed8948a22
gaceao <116834172+gaceao@users.noreply.github.com>
2022-12-28 11:27:33 +0100

    actualizacion

--- Modelo H8/1 GDL/linealizacion_hemsH8.m
+++ Modelo H8/1 GDL/linealizacion_hemsH8.m
@@ -4,13 +4,13 @@ load("force_airgap_current.mat");
 current_v = current(1,:);
 airgap_v = airgap(:,1);
 
-lev_nom = 0.019;
+lev_nom = 0.01965;
 g=9.81;
 m=200;
 
 %% Con LUT
 
-x=[0.019, 0]; % estados
+x=[0.01965, 0]; % estados
 argout=linmod('H8_1GDL_currentinput',x,0); % entrada
 A=argout.a;
 B=argout.b;
