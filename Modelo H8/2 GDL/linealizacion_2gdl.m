8d6b1a86916b3c5938b849016e0f66e0878850ab
gaceao <116834172+gaceao@users.noreply.github.com>
2023-01-22 14:00:15 +0100

    modelo lineal 2gdl

--- Modelo H8/2 GDL/linealizacion_2gdl.m
+++ Modelo H8/2 GDL/linealizacion_2gdl.m
@@ -1,9 +1,9 @@
 x=[0,0.01965, 0, 0];  %estados
 argout=linmod('H8_2GDL_partelinealizada',x,[0,0]); % entrada
-A2=argout.a;
-B2=argout.b;
-C2=argout.c;
-D2=argout.d;
+A2=argout.a
+B2=argout.b
+C2=argout.c
+D2=argout.d
 sys=ss(A2,B2,C2,D2);
 %sysr=minreal(sys)
 %C esta OK con [1 0 0 0; 0 1 0 0]
