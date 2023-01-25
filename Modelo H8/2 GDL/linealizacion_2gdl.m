27b04624f5658314595ebac4f07f533d4bd60e48
Raquel Murcia <68744281+ramurjua@users.noreply.github.com>
2023-01-25 22:28:44 +0100

    2 gdl: modelo no-lineal y control-lineal

--- Modelo H8/2 GDL/linealizacion_2gdl.m
+++ Modelo H8/2 GDL/linealizacion_2gdl.m
@@ -1,5 +1,5 @@
 x=[0, 0.01965, 0, 0];  %estados
-argout=linmod('H8_2GDL_partelinealizada',x,[0,0]); % entrada
+argout=linmod('H8_2GDL_partelinealizada',x,[0, 0]); % entrada
 A2=argout.a
 B2=argout.b
 C2=argout.c
@@ -8,3 +8,10 @@ sys=ss(A2,B2,C2,D2);
 %sysr=minreal(sys)
 %C esta OK con [1 0 0 0; 0 1 0 0]
 %D esta OK con 0
+
+%salida 1: theta
+%salida 2: zpos
+%salida 3: theta derivada
+%salida 4: zpos derivada
+
+K = place(sys.A,sys.B,[-2,-3,-4,-5])
