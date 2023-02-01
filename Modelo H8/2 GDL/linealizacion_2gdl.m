3fd3ea55ecea2097113e4d95c57315545701cea5
Raquel Murcia <68744281+ramurjua@users.noreply.github.com>
2023-02-01 13:08:42 +0100

    modelo 2gdl cambios

--- Modelo H8/2 GDL/linealizacion_2gdl.m
+++ Modelo H8/2 GDL/linealizacion_2gdl.m
@@ -1,18 +1,13 @@
-x=[0.01965, 0.01965, 0, 0];  %estados
+x=[0, lev_nom, 0, 0];  %estados
 argout=linmod('H8_2GDL_partelinealizada',x,[0, 0]); % entrada
 A2=argout.a
 B2=argout.b
 C2=argout.c
 D2=argout.d
 sys=ss(A2,B2,C2,D2);
-%sysr=minreal(sys)
-%C esta OK con [1 0 0 0; 0 1 0 0]
-%D esta OK con 0
 
-%salida 1: airgap1
-%salida 2: airgap2
 
-%Ka = place(sys.A,sys.B,[-2,-3,-4,-5, -6, -7])
+K = place(sys.A,sys.B,[-2,-3,-4,-5])
 
 
 
