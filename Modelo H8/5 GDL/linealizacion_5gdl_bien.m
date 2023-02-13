d241b3e061e77ecbd29ce85089c8c7b5acfa10eb
pech422 <alvarosanjo04@gmail.com>
2023-02-13 10:43:50 +0100

    update 5gdl lineal

--- Modelo H8/5 GDL/linealizacion_5gdl_bien.m
+++ Modelo H8/5 GDL/linealizacion_5gdl_bien.m
@@ -2,16 +2,16 @@
   lev_nom=0.01965;
   cg_suelo=0.56985;
   cg_techo=0.55735;
-  x=[0, 0, cg_nom, 0, 0, 0, 0, 0, 0, 0, 0, 0]; %estados: posx,posy,posz,rotx,roty,rotz,vx,vy,vz,wx,wy,wz
-  argout=linmod('H8_6GDL_partelinealizada',x,[0, 0, 0, 0, 0, 0, 0, 0]); %entradas
-  A5=argout.a
-  B5=argout.b
-  C5=argout.c
-  D5=argout.d
+%    x=[0, 0, cg_nom, 0, 0, 0, 0, 0, 0, 0, 0, 0]; %estados: posx,posy,posz,rotx,roty,rotz,vx,vy,vz,wx,wy,wz
+%    argout=linmod('H8_6GDL_partelinealizada',x,[0, 0, 0, 0, 0, 0, 0, 0]); %entradas
+%    A5=argout.a
+%    B5=argout.b
+%    C5=argout.c
+%    D5=argout.d
 % sys=ss(A5,B5,C5,D5);
 %rank(ctrb(A5,B5))
 %eig(A5)
-%K = place(A6,B6,[-1 -1 -1 -1 -2 -2 -2 -2 -2+2j -2-2j])
+K = place(A6,B6,[-1 -1 -1 -1 -2 -2 -2 -2 -2+2j -2-2j])
 %Si cambian las matrices hay que eliminar columnas 1 y 7 y filas 1 y 7
 
 %% Añadir accion integral
@@ -20,10 +20,10 @@
 %C = [1 0 0 0; 0 1 0 0];
 %D = zeros(2,2)
 
-Aa = [A6 zeros(10,10); C6 zeros(10,10)] 
-Ba = [B6; D6]
+Aa = [A5 zeros(10,10); C5 zeros(10,10)] 
+Ba = [B5; D5]
 rank(ctrb(Aa,Ba))
 
-Ka = place(Aa,Ba,[ -1.5 -1.5 -1.5 -1.5 -2 -2 -6 -6 -7 -7 -5 -5 -2 -2 -3 -3 -4 -4 -6+6j -6-6j])
+Ka = place(Aa,Ba,[ -1 -1 -2 -2 -3 -3 -6 -6 -7 -7 -5 -5 -4 -4 -5 -5 -8 -8 -6+6j -6-6j])
 
 %Ka = place(Aa,Ba,[-5 -4 -6+6j -6-6j -2+2j -2-2j])
