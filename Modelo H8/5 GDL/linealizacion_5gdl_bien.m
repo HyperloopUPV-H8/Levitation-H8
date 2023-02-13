e17c55e24e0ce4e6e43e2de5956fa07b13460aab
pech422 <alvarosanjo04@gmail.com>
2023-02-13 09:55:54 +0100

    Control del no lineal en el lineal

--- Modelo H8/5 GDL/linealizacion_5gdl_bien.m
+++ Modelo H8/5 GDL/linealizacion_5gdl_bien.m
@@ -1,17 +1,17 @@
-%  cg_nom=0.567;
-%  lev_nom=0.01965;
-%  cg_suelo=0.56985;
-%  cg_techo=0.55735;
-%  x=[0, 0, cg_nom, 0, 0, 0, 0, 0, 0, 0, 0, 0]; %estados: posx,posy,posz,rotx,roty,rotz,vx,vy,vz,wx,wy,wz
-%  argout=linmod('H8_6GDL_partelinealizada',x,[0, 0, 0, 0, 0, 0, 0, 0]); %entradas
-%  A5=argout.a
-%  B5=argout.b
-%  C5=argout.c
-%  D5=argout.d
+  cg_nom=0.567;
+  lev_nom=0.01965;
+  cg_suelo=0.56985;
+  cg_techo=0.55735;
+  x=[0, 0, cg_nom, 0, 0, 0, 0, 0, 0, 0, 0, 0]; %estados: posx,posy,posz,rotx,roty,rotz,vx,vy,vz,wx,wy,wz
+  argout=linmod('H8_6GDL_partelinealizada',x,[0, 0, 0, 0, 0, 0, 0, 0]); %entradas
+  A5=argout.a
+  B5=argout.b
+  C5=argout.c
+  D5=argout.d
 % sys=ss(A5,B5,C5,D5);
 %rank(ctrb(A5,B5))
 %eig(A5)
-K = place(A6,B6,[-1 -1 -1 -1 -2 -2 -2 -2 -2+2j -2-2j])
+%K = place(A6,B6,[-1 -1 -1 -1 -2 -2 -2 -2 -2+2j -2-2j])
 %Si cambian las matrices hay que eliminar columnas 1 y 7 y filas 1 y 7
 
 %% Añadir accion integral
