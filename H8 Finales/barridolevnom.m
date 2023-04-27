9d1545edcc79cec87c2a6439571b1064005d7093
gaceao <116834172+gaceao@users.noreply.github.com>
2023-04-27 12:56:06 +0200

    1gdl masa variable ajustado

--- H8 Finales/barridolevnom.m
+++ H8 Finales/barridolevnom.m
@@ -1,12 +1,15 @@
-%load("hems.mat");
-format long;
-for i = 185:5:200
 
-        m = i;
-        m_1gdl = m/4;
+format long;
+load('hems.mat')
+alturas = [];
+masa = 165:1:200;
+for i = 165:1:200
+
+        m_1gdl = i/4;
         index=find(hems.Current == 0);
         p = polyfit(hems.Force_Vertical(:,index), hems.Airgap', 5);
         lev_nom = polyval(p,m_1gdl*9.81)/1000;
-        sim("H8_1GDL_barridomasa.slx");
+        alturas = horzcat(alturas,lev_nom);
+        %sim("H8_1GDL_barridomasa.slx");
 
 end
