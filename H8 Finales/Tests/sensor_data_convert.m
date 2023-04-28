2bbce3e63fd90e3a2aa29449db9fda5ce87b8a3e
gaceao <116834172+gaceao@users.noreply.github.com>
2023-04-28 21:17:20 +0200

    recolocacion pruebas barrido masa

--- H8 Finales/Tests/sensor_data_convert.m
+++ H8 Finales/Tests/sensor_data_convert.m
@@ -1,12 +1,12 @@
 function [values, times] = sensor_data_convert(csvfile)
     
-    values = csvfile(:,2);
-    times = csvfile(:,1);
-    
-    figure
-    plot(times, values)
-    title("Recorded data")
-    xlabel("Time")
-    ylabel("Value")
+    values = csvfile(:,5);
+    times = csvfile(:,2);
+    str2num()
+    %figure
+    %plot(times, values)
+    %title("Recorded data")
+    %xlabel("Time")
+    %ylabel("Value")
 
 end
